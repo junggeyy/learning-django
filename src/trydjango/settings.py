41dec7463dcaa0ae43a00cf8b72673270f0eb256
junggeyy <jung.vickey@gmail.com>
2025-03-18 15:26:31 -0400

    models, custom page, views, urls and template

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # my apps
     'products',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "template")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
